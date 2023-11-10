495d13bf510b4bb121f0a460a5a9856ebb7b92a8
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-11-11 04:06:27 +0900

    solve : p02c

---
+++ PS/PS1/problem_2c.md
@@ -0,0 +1,16 @@
+# 问题 (c)
+Write a logistic regression classifier that uses
+$x_1$ and $x_2$ as input features, and train it using the t-labels.
+
+
+## Shape
+Here, I'm going to add intercept for convention.
+```bash
+train_x.shape :  (1250, 3)
+train_t.shape :  (1250,)
+```
+
+Just let you know, I got same result regardless of intercept.  
+
+## Model
+In this problem, we are going to inherit logitic model of problem1b, which uses Newton's method to fit.
--- PS/PS1/src/p02cde_posonly.py
+++ PS/PS1/src/p02cde_posonly.py
@@ -28,6 +28,22 @@ def main(train_path, valid_path, test_path, pred_path):
     # *** START CODE HERE ***
     # Part (c): Train and test on true labels
     # Make sure to save outputs to pred_path_c
+    ## Step0 : check the size of datas
+    train_x, train_t = util.load_dataset(train_path,label_col='t',add_intercept=True)
+        # print("train_x.shape : ",train_x.shape)
+        # print("train_t.shape : ",train_t.shape)
+    
+    ## Step1 : Train
+    clf = LogisticRegression()
+    clf.fit(train_x, train_t)
+    
+    ## Step2 : Test (predict)
+    test_x, _ = util.load_dataset(test_path,add_intercept=True)
+    result_pred = clf.predict(test_x)
+    
+    ## Step3 : save outputs
+    np.savetxt(pred_path_c,result_pred,'%d')
+    
     # Part (d): Train on y-labels and test on true labels
     # Make sure to save outputs to pred_path_d
     # Part (e): Apply correction factor using validation set and test on true labels
